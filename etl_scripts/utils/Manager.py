259c67d745447262c6795cbde396d82e9a451ea0
andresfeliper3 <andresfeliper.2001@gmail.com>
2023-11-27 13:44:30 -0500

    Nombre de proceso añadido al log

--- etl_scripts/utils/Manager.py
+++ etl_scripts/utils/Manager.py
@@ -17,32 +17,32 @@ class Manager:
         for extractor in self.extractors:
             try:
                 extractor.extract()
-                logger_process.info(f"Table {extractor.get_table_name()} extracted")
+                logger_process.info(f"Process {self.manager_name}: table {extractor.get_table_name()} extracted")
                 extracted_table = extractor.get_table()
                 self.tables.append(extracted_table)
-                logger_tables_extract.info(f"Table {extractor.get_table_name()} extracted")
+                logger_tables_extract.info(f"Process {self.manager_name}: table {extractor.get_table_name()} extracted")
                 logger_tables_extract.info(etl.head(extracted_table))
             except Exception as e:
-                logger_process.error(f'Error in extracting data from {extractor.get_table_name()} table: {e}',
+                logger_process.error(f'Error in process {self.manager_name} extracting data from {extractor.get_table_name()} table: {e}',
                                      exc_info=True)
 
     def check_if_extractors_available(self):
         if not self.extractors:
-            logger_process.warn("No extractions defined")
+            logger_process.warn(f"Process {self.manager_name}: no extractions defined")
 
     def transform(self):
-        logger_process.info(f"Transformation in progress for {self.loader.get_table_name()}")
+        logger_process.info(f"Process {self.manager_name}: transformation in progress for {self.loader.get_table_name()}")
         self.transformer.transform(self.tables)
         self.transformed_table = self.transformer.get_transformed_table()
-        logger_tables_transform.info(f"Transformation in progress for {self.loader.get_table_name()}")
+        logger_tables_transform.info(f"Process {self.manager_name}: Transformation in progress for {self.loader.get_table_name()}")
         logger_tables_transform.info(self.transformed_table)
-        logger_process.info("Transformation complete")
+        logger_process.info(f" Process {self.manager_name}:Transformation complete")
 
     def load(self):
         self.loader.load(self.transformed_table)
         logger_tables_load.info(self.transformed_table)
-        logger_process.info(f"Table {self.loader.get_table_name()} loaded!")
-        logger_tables_load.info(f"Table {self.loader.get_table_name()} loaded")
+        logger_process.info(f"Process {self.manager_name}: table {self.loader.get_table_name()} loaded!")
+        logger_tables_load.info(f"Process {self.manager_name}: table {self.loader.get_table_name()} loaded")
 
     def execute_phases(self, execute_extract: bool, execute_transform: bool, execute_load: bool):
         if execute_extract:
